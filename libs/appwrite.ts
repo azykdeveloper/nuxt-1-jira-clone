46cb0be825f4f8bce2831a228d334ff6c825fca3
azykdeveloper <azykdeveloper@gmail.com>
2025-06-05 23:46:53 +0500

    c

--- libs/appwrite.ts
+++ libs/appwrite.ts
@@ -1,11 +1,13 @@
 import { Client, Account, ID } from "appwrite";
 
-const url: string = import.meta.env.VITE_APPWRITE_ENDPOINT;
-const project: string = import.meta.env.VITE_APPWRITE_PROJECT;
+// const url: string = import.meta.env.NUXT_PUBLIC_APPWRITE_ENDPOINT;
+// const project: string = import.meta.env.NUXT_PUBLIC_APPWRITE_PROJECT;
 
 const client: Client = new Client();
 
-client.setEndpoint(url).setProject(project);
+client
+  .setEndpoint("https://fra.cloud.appwrite.io/v1")
+  .setProject("684121360008db279808");
 
-export const ACCOUNT = new Account(client);
-export const IDENTITY = ID.unique();
+export const ACCOUNT: Account = new Account(client);
+export const IDENTITY: string = ID.unique();
